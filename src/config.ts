566b26c069b48946d23d3d235de9fbd5a0945807
Mato <codeknight4242@yahoo.ca>
2025-07-08 09:36:50 -0400

    config logo name fix

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/logo-mato.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/mato-logo.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Rowan Mato",
 	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	links: [

310d0aa1d276fc87f65d8bfcc24c62add06663a9
Mato <codeknight4242@yahoo.ca>
2025-07-09 06:43:25 -0400

    dummy about page

--- src/i18n/languages/en.ts
+++ src/i18n/languages/en.ts
@@ -5,7 +5,7 @@ export const en: Translation = {
 	[Key.home]: "Home",
 	[Key.about]: "About",
 	[Key.archive]: "Archive",
-	[Key.archive]: "Projects",
+	[Key.projects]: "Projects",
 	[Key.search]: "Search",
 
 	[Key.tags]: "Tags",

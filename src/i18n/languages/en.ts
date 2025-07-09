2c45fa49cfdb15e1f88f32aecd79102ae235c576
Mato <codeknight4242@yahoo.ca>
2025-07-09 06:26:27 -0400

    Partial setup for Projects page. Gotta learn this later.

--- src/i18n/languages/en.ts
+++ src/i18n/languages/en.ts
@@ -5,6 +5,7 @@ export const en: Translation = {
 	[Key.home]: "Home",
 	[Key.about]: "About",
 	[Key.archive]: "Archive",
+	[Key.archive]: "Projects",
 	[Key.search]: "Search",
 
 	[Key.tags]: "Tags",

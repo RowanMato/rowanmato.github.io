b6bb426fb2df2245ab613070fe8f2d876b6b6d96
Mato <codeknight4242@yahoo.ca>
2025-07-09 03:28:16 -0400

    Drafted all example posts, changed color, added my own images, etc

--- src/config.ts
+++ src/config.ts
@@ -12,12 +12,12 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Rowan Mato's homepage",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 125, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "assets/images/river-banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/mato-logo.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/pngtuber.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Rowan Mato",
 	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	links: [
